eb39a9931b667e74c0eabffd26bf8900858aa5a5
Dmitry Erlikh <derlih@gmail.com>
2025-05-26 14:16:19 +0200

    Native no mem leak test (#161)
    * use custom allocator for containers
    
    * native mem leak detection test

--- tests/test_native.py
+++ tests/test_native.py
@@ -1,5 +1,7 @@
+import gc
 from typing import Any
 
+import psutil
 import pytest
 from levdist.native import wagner_fischer_native
 
@@ -17,3 +19,22 @@ from levdist.native import wagner_fischer_native
 def test_native_wrong_arguments(params: Any) -> None:  # noqa: ANN401
     with pytest.raises(TypeError):
         wagner_fischer_native(*params)
+
+
+def test_native_no_mem_leak() -> None:
+    # Warm up
+    wagner_fischer_native("a-dog", "cat-b")
+
+    process = psutil.Process()
+    gc.collect()
+
+    before = process.memory_info().rss
+    iterations = 10_000_000
+
+    for i in range(iterations):
+        wagner_fischer_native(f"{i}-dog", "cat-{i}")
+
+    gc.collect()
+    after = process.memory_info().rss
+
+    assert after - before < iterations
