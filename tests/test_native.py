82ab3449bf9ee62cc4ae11b3e427cb5a4319b7d7
Dmitry Ehrlich <derlih@gmail.com>
2026-06-05 18:27:53 +0200

    adjust test_native_no_mem_leak test (#221)
    * adjust test_native_no_mem_leak test
    
    * refactor memory leak tests to use tracemalloc and separate RSS checks
    
    * fix memory leak tests to use defined constants for memory limits

--- tests/test_native.py
+++ tests/test_native.py
@@ -5,6 +5,19 @@ import psutil
 import pytest
 from levdist._native import wagner_fischer_native
 
+try:
+    import tracemalloc
+
+    TRACEMALLOC_AVAILABLE = True
+    MAX_MEMORY_DIFF_TRACEMALLOC = 1 * 1024 * 1024
+
+except ImportError:
+    TRACEMALLOC_AVAILABLE = False
+
+
+# RSS memory can be more variable
+MAX_MEMORY_DIFF_RSS = 10 * 1024 * 1024
+
 
 @pytest.mark.parametrize(
     "params",
@@ -21,20 +34,33 @@ def test_native_wrong_arguments(params: Any) -> None:  # noqa: ANN401
         wagner_fischer_native(*params)
 
 
-def test_native_no_mem_leak() -> None:
-    # Warm up
+@pytest.mark.skipif(not TRACEMALLOC_AVAILABLE, reason="tracemalloc not available")
+def test_native_no_mem_leak_tracemalloc() -> None:
+    wagner_fischer_native("a-dog", "cat-b")
+
+    tracemalloc.start()
+    tracemalloc.clear_traces()
+
+    for i in range(100_000):
+        wagner_fischer_native(f"{i}-dog", f"cat-{i}")
+
+    _, peak_traced = tracemalloc.get_traced_memory()
+    tracemalloc.stop()
+
+    assert peak_traced < MAX_MEMORY_DIFF_TRACEMALLOC
+
+
+def test_native_no_mem_leak_rss() -> None:
     wagner_fischer_native("a-dog", "cat-b")
 
     process = psutil.Process()
     gc.collect()
+    before_rss = process.memory_info().rss
 
-    before = process.memory_info().rss
-    iterations = 10_000_000
-
-    for i in range(iterations):
-        wagner_fischer_native(f"{i}-dog", "cat-{i}")
+    for i in range(100_000):
+        wagner_fischer_native(f"{i}-dog", f"cat-{i}")
 
     gc.collect()
-    after = process.memory_info().rss
+    after_rss = process.memory_info().rss
 
-    assert after - before < iterations
+    assert after_rss - before_rss < MAX_MEMORY_DIFF_RSS
